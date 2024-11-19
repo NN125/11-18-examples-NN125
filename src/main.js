4a2d0a0fe8e72cf7a68b1a4e2f9a9f1f747e27b5
NN125 <nicolas.nolden@student.swic.edu>
2024-11-18 20:18:37 -0600

    Add createElement and createTextNode examples from MDN

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@
 const paragraphs = document.querySelectorAll("p"); //checks the html for every instance of the p tag
 
 console.log(Array.from(paragraphs).map((pTag) => pTag.innerText)); //Array.from(paragraphs) converts paragraphs into an array so we could use .map
+
+const heading = document.createElement("h1");
+const headingText = document.createTextNode("Big Head!");
+heading.appendChild(headingText);
+document.body.appendChild(heading);
