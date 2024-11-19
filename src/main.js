70045e76eac67692e2fc97978967739ae9b69bb0
NN125 <nicolas.nolden@student.swic.edu>
2024-11-18 20:21:46 -0600

    Add note comment

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ const paragraphs = document.querySelectorAll("p"); //checks the html for every i
 console.log(Array.from(paragraphs).map((pTag) => pTag.innerText)); //Array.from(paragraphs) converts paragraphs into an array so we could use .map
 
 document.body.appendChild(document.createElement("h2")).textContent =
-  "Hello World!";
+  "Hello World!"; //appendChild adds the resulting createElement and textContent to the html on runtime
