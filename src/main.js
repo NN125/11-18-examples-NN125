3c980029c6973b195d60663a07185058edbcfc67
NN125 <nicolas.nolden@student.swic.edu>
2024-11-18 20:20:28 -0600

    Replace MDN example with a shorter way to write it

--- src/main.js
+++ src/main.js
@@ -3,7 +3,5 @@ const paragraphs = document.querySelectorAll("p"); //checks the html for every i
 
 console.log(Array.from(paragraphs).map((pTag) => pTag.innerText)); //Array.from(paragraphs) converts paragraphs into an array so we could use .map
 
-const heading = document.createElement("h1");
-const headingText = document.createTextNode("Big Head!");
-heading.appendChild(headingText);
-document.body.appendChild(heading);
+document.body.appendChild(document.createElement("h2")).textContent =
+  "Hello World!";
