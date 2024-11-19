d4cf68602cfc4b6704b69138b553ca559e785b7c
NN125 <nicolas.nolden@student.swic.edu>
2024-11-18 20:03:24 -0600

    Add p tag query

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
 // TODO: Do what must be done
+const paragraphs = document.querySelectorAll("p"); //checks the html for every instance of the p tag
+
+console.log(Array.from(paragraphs).map((pTag) => pTag.innerText)); //Array.from(paragraphs) converts paragraphs into an array so we could use .map
